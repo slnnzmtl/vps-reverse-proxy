0b6e4a98f718fae69e42a79aa58d357890d7c311
slnnzmtl <slonanezametil@gmail.com>
2025-08-18 23:35:46 +0000

    Update Docker Compose and renewal script: add certbot service and modify certificate renewal process

--- renew-certs.sh
+++ renew-certs.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Renew certificates
-certbot renew
+# Renew certificates using docker
+docker run --rm -v /root/containers/reverse-proxy/certbot/conf:/etc/letsencrypt -v /root/containers/reverse-proxy/certbot/www:/var/www/certbot certbot/certbot renew
 
-# Copy certificates to the correct locations
-cp /etc/letsencrypt/live/crm.slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/crm.slnnzmtl.xyz/
-cp /etc/letsencrypt/live/crm.slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/crm.slnnzmtl.xyz/
-cp /etc/letsencrypt/live/crm.slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/slnnzmtl.xyz/
-cp /etc/letsencrypt/live/crm.slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/slnnzmtl.xyz/
-
+# Copy certificates to the correct locations (all domains use the same certificate from slnnzmtl.xyz)
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/crm.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/crm.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/n8n.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/n8n.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/dev.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/dev.slnnzmtl.xyz/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/fullchain.pem /root/containers/reverse-proxy/ssl/kazansky.dev/
+cp /root/containers/reverse-proxy/certbot/conf/live/slnnzmtl.xyz/privkey.pem /root/containers/reverse-proxy/ssl/kazansky.dev/
 # Restart the reverse proxy
 cd /root/containers/reverse-proxy
 docker-compose restart reverse-proxy 
